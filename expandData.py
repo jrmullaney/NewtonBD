570b51399f566d62adf2836c35ccc511f9fd3c5b
James Mullaney <Home@JRMBook.lan>
2018-02-04 20:12:29 +0000

    Now reading csv files, rather than fits

--- expandData.py
+++ expandData.py
@@ -1,13 +1,13 @@
 from astropy.table import Table, Column, vstack
 import numpy as np
-tab = Table.read('tableDiff.fits')
+tab = Table.read('tableDiff.csv')
 
 btab = vstack([tab,tab])
-for i in range(100):
+for i in range(5):
     btab = vstack([btab,tab])
 
 ind = np.arange(1,len(btab)+1)
 c = Column(data=ind, dtype=np.dtype('u4'), name="index")
 btab.add_column(c, index=0)
 
-btab.write("tableDiff.csv", overwrite=True)
+btab.write("tableDiffBig.csv", overwrite=True)
